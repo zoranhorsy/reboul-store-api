82c62e4d43bcbe4d1d0d5ddaea1f63c9434162a9
zoran <zxransounds@gmail.com>
2025-03-31 16:15:24 +0200

    fix: update SMTP password environment variable name from SMTP_PASS to SMTP_PASSWORD

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@ const transporter = nodemailer.createTransport({
     secure: process.env.SMTP_SECURE === 'true',
     auth: {
         user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
+        pass: process.env.SMTP_PASSWORD,
     },
 });
 
